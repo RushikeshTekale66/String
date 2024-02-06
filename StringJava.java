e5887b4430a0effa15e4113e2426f6db0072687c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-06 20:06:09 +0530

    String input nextLine() & next()

--- StringJava.java
+++ StringJava.java
@@ -1,14 +1,17 @@
 import java.util.*;
-//Get string input from user
+//Reverse the string 
 class StringJava{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the String");
-        String str = sc.nextLine();
-        String str2 = "";
-        for(int i=0; i<str.length(); i++){
-            str2 =str.charAt(i)+str2 ;
-        }
+
+        //Input "Rushikesh Tekale" Output "Rushikesh Tekale"
+        System.out.println("Enter the 1 String");
+        String str1 = sc.nextLine();
+        System.out.println(str1);
+       
+        //Input "Rushikesh Tekale" Output "Rushikesh"
+        System.out.println("Enter the 2 String");
+        String str2 = sc.next();
         System.out.println(str2);
     }
 }
