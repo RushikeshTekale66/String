06f79b3f3be1e0fd7c3e697b5a33d509eb7b0dcc
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-06 20:32:21 +0530

    String Function: str1.indexOf(str2)

--- StringJava.java
+++ StringJava.java
@@ -4,8 +4,12 @@ class StringJava{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        String str = "Rushikesh Tekale";
-        //It will return the first index of that character
-        System.out.println(str.indexOf('h'));
+        String str1 = "Rushikesh Tekale";
+        String str2 = "Tekale";
+        
+        //str1 is the string in which you want to search for str2.
+        //str2 is the substring that you want to find within str1.
+        //It will compre str2 in str1 and return the first index of the str2
+        System.out.println(str1.indexOf(str2));
     }
 }
