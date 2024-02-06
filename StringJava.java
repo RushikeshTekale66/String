98febc2969646e651f26fe7f8a89fc1c6abc2cf4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-06 21:02:19 +0530

    Special Character in String

--- StringJava.java
+++ StringJava.java
@@ -4,10 +4,19 @@ class StringJava{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        String str1 = "Rushikesh Tekale ";
-        String str2 = "Tekale Rushikesh";
-        
-        //It will concatinate the two string
-        System.out.println(str1.concat(str2));
+        //Special Character in java
+
+        //1 New Line
+        System.out.println("Rushikesh \n Tekale");
+
+        //2 Tab
+        System.out.println("Rushikesh \t Tekale");
+
+        //3 Single Backslash
+        System.out.println("Rushikesh \\ Tekale");
+
+        //4 BackSpace
+        System.out.println("Rushikesh\bh Tekale");
+
     }
 }
