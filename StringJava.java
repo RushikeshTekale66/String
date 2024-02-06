0268412cf841502f9f9fb07b5650b8f9dd8172a2
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-06 20:20:49 +0530

    String Function: str.toUpperCase()

--- StringJava.java
+++ StringJava.java
@@ -5,7 +5,7 @@ class StringJava{
         Scanner sc = new Scanner(System.in);
         
         String str = "Rushikesh Tekale";
-        //It will give the range of string from 2 to 8 insted of 2 to 9
-        System.out.println(str.substring(2, 9));
+        //Convert all string in uppercase
+        System.out.println(str.toUpperCase());
     }
 }
