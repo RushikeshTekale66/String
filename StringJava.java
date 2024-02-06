e8c50a9acf5990762ac249c695fbb701e6e90154
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-06 20:09:48 +0530

    String Method: str.length()

--- StringJava.java
+++ StringJava.java
@@ -3,15 +3,8 @@ import java.util.*;
 class StringJava{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        //Input "Rushikesh Tekale" Output "Rushikesh Tekale"
-        System.out.println("Enter the 1 String");
-        String str1 = sc.nextLine();
-        System.out.println(str1);
-       
-        //Input "Rushikesh Tekale" Output "Rushikesh"
-        System.out.println("Enter the 2 String");
-        String str2 = sc.next();
-        System.out.println(str2);
+        
+        String str = "Rushikesh Tekale";
+        System.out.println(str.length());
     }
 }
