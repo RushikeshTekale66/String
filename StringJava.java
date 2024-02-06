3e4530598573f2f66a6c3c85322cdd127faaf9f3
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-06 20:45:07 +0530

    String Function: str1.concat(str2)

--- StringJava.java
+++ StringJava.java
@@ -4,10 +4,10 @@ class StringJava{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        String str1 = "Rushikesh Tekale";
+        String str1 = "Rushikesh Tekale ";
         String str2 = "Tekale Rushikesh";
         
-        //The equals() method returns true if the two strings contain the same sequence of characters; otherwise, it returns false.
-        System.out.println(str1.equals(str2));
+        //It will concatinate the two string
+        System.out.println(str1.concat(str2));
     }
 }
