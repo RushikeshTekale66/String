c466100450fb5c486298296b55838f0b9b1d5436
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-10 22:01:00 +0530

    String Declaration

--- StringJava.java
+++ StringJava.java
@@ -2,21 +2,7 @@ import java.util.*;
 //Reverse the string 
 class StringJava{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        //Special Character in java
-
-        //1 New Line
-        System.out.println("Rushikesh \n Tekale");
-
-        //2 Tab
-        System.out.println("Rushikesh \t Tekale");
-
-        //3 Single Backslash
-        System.out.println("Rushikesh \\ Tekale");
-
-        //4 BackSpace
-        System.out.println("Rushikesh\bh Tekale");
-
+        String str = "Rushikesh Tekale";
+        System.out.println(str);
     }
 }
