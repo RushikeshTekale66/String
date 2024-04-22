a4eccb12005a540976f42736ff31d708492e204b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-04-22 20:49:40 +0530

    String Reverse

--- StringJava.java
+++ StringJava.java
@@ -2,11 +2,13 @@ import java.util.*;
 //Contat two string without any space
 class StringJava{
     public static void main(String[] args) {
-        String str = "Tekale";
-        String str2 = "Tekale";
-        String str3 = "Rushikesh";
-        // if both are equal return 0 else return any integer number
-        System.out.println(str.compareTo(str2));
-        System.out.println(str.compareTo(str3));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Strign: ");
+        String str = sc.nextLine();
+        String str2="";
+        for(int i=str.length()-1; i>=0; i--){
+                str2 = str2+str.charAt(i);
+        }   
+        System.out.println("Reversed String is: " + str2);
     }
 }
