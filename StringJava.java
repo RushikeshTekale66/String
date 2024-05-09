01244a388b2b3ed894b0dd00d6630163d4afacb2
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-09 21:51:20 +0530

    Find Common Prefix

--- StringJava.java
+++ StringJava.java
@@ -1,14 +1,42 @@
+// Java program to find longest common prefix of
+// given array of words.
 import java.util.*;
-//Contat two string without any space
-class StringJava{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Strign: ");
-        String str = sc.nextLine();
-        String str2="";
-        for(int i=str.length()-1; i>=0; i--){
-                str2 = str2+str.charAt(i);
-        }   
-        System.out.println("Reversed String is: " + str2);
+
+public class StringJava
+{
+    public String longestCommonPrefix(String[] a)
+    {
+        int size = a.length;
+
+        /* if size is 0, return empty string */
+        if (size == 0)
+            return "";
+
+        if (size == 1)
+            return a[0];
+
+        /* sort the array of strings */
+        Arrays.sort(a);
+
+        /* find the minimum length from first and last string */
+        int end = Math.min(a[0].length(), a[size-1].length());
+
+        /* find the common prefix between the first and
+           last string */
+        int i = 0;
+        while (i < end && a[0].charAt(i) == a[size-1].charAt(i) )
+            i++;
+
+        String pre = a[0].substring(0, i);
+        return pre;
+    }
+
+    /* Driver Function to test other function */
+    public static void main(String[] args)
+    {
+        StringJava gfg = new StringJava();
+        String[] input = {"geeksforgeeks", "geeks", "geek", "geezer"};
+        System.out.println( "The longest Common Prefix is : " +
+                                   gfg.longestCommonPrefix(input));
     }
 }
