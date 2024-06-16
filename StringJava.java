d3d6ec08fb68ef14a17e3bff5009771f2cb88b0e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-16 18:26:54 +0530

    Remove the white spaces from the string

--- StringJava.java
+++ StringJava.java
@@ -3,11 +3,17 @@
  */
 public class StringJava {
 
-    // remove the special characters form the string
+    // remove the white spaces from string
     public static void main(String [] args){
-        String str = "sifn<>!@$@dfinf@#$@#kdfnQAAd#$#$23131313131";
-        // Replace the character other than [^a-zA-Z0-9]
-        String newSTring = str.replaceAll("[^a-zA-Z0-9]", "");
-        System.out.println(newSTring);
+        String str = "a b c d f  A  B  C  D  F";
+        
+        // remove the white space form string
+        String newString = str.replaceAll("\\s", "");
+        System.out.println("New String : " + newString);
+
+        String str2 = "        a      b     ";
+        System.out.println("Original String : " + str2);
+        // Trim method remove the white spaces from both the end of the string
+        System.out.println("Trim String : " + str2.trim());
     }
 }
