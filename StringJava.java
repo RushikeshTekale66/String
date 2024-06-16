aa4255b2b6717b71479220ce724bd50cf3b288ac
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-16 19:11:51 +0530

    Remove duplicate character from string

--- StringJava.java
+++ StringJava.java
@@ -1,19 +1,59 @@
+import java.util.*;
+
 /**
  * StringJava
  */
 public class StringJava {
 
-    // remove the white spaces from string
+    // remove duplicate character from string
     public static void main(String [] args){
-        String str = "a b c d f  A  B  C  D  F";
-        
-        // remove the white space form string
-        String newString = str.replaceAll("\\s", "");
-        System.out.println("New String : " + newString);
+        String str = "AABBCCDDaabbccdd";
 
-        String str2 = "        a      b     ";
-        System.out.println("Original String : " + str2);
-        // Trim method remove the white spaces from both the end of the string
-        System.out.println("Trim String : " + str2.trim());
+        // Approach 1 using String builder
+        StringBuilder sb1 = new StringBuilder();
+        str.chars().distinct().forEach(c -> sb1.append((char)c));
+        System.out.println("Using String builder : " + sb1);
+
+        // Approach 2 using indexOf() method
+        StringBuilder sb2 = new StringBuilder();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            int index = str.indexOf(ch, i+1);
+            if(index==-1){
+                sb2.append(ch);
+            }
+        }
+        System.out.println("Using IndexOf() method : "+sb2);
+
+        // Approach 3 converting string string in charArray
+        StringBuilder sb3 = new StringBuilder();
+        char[] arr = str.toCharArray();
+
+        for(int i=0; i<arr.length; i++){
+            boolean repited = false;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]==arr[j]){
+                    repited = true;
+                    break;
+                }
+            }
+            if(!repited){
+                sb3.append(arr[i]);
+            }
+        }
+        System.out.println("Using toCharArray() : "+ sb3);
+
+        // Approach 4 using set interface
+        StringBuilder sb4 = new StringBuilder();
+        Set<Character> set = new LinkedHashSet<>();
+
+        for(int i=0; i<str.length(); i++){
+            set.add(str.charAt(i));
+        }
+        for(Character c : set){
+            sb4.append(c);
+        }
+        System.out.println("Using set interface : "+ sb4);
+
     }
 }
