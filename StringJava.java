599c672d4bbc74ab0957ccf48dc023de299086e2
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-13 11:41:19 +0530

    Total count of vovel persent in the String

--- StringJava.java
+++ StringJava.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
-class CountEachVovel{
+class StringJava{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter the String to check");
 		String name = sc.next();
 		name = name.toLowerCase();
-	
+		int count = 0;
+
+		//Traditional
+		for(int i=0; i<name.length(); i++){
+			if(name.charAt(i)=='a' || name.charAt(i)=='e' || name.charAt(i)=='i' || name.charAt(i)=='o' || name.charAt(i)=='u'){
+				count++;
+			}
+		}
+		System.out.println("Count of Vovel is " + count);
+
+
+		//Advance Option		
 		String vovel = "aeiou";
-		for(int i=0; i<vovel.length(); i++){
-			int count2 = 0;
-			for(int j=0; j<name.length(); j++){
-				if(name.charAt(j)==vovel.charAt(i)){
+		int count2 = 0;
+		for(int i=0; i<name.length(); i++){
+			for(int j=0; j<vovel.length(); j++){
+				if(name.charAt(i)==vovel.charAt(j)){
 					count2++;
 				}
 			}
-		System.out.println("Count of vovel " + vovel.charAt(i) + " is " + count2);
 		}	
+		System.out.println("Count of vovel : " + count2);
 	}
 }
