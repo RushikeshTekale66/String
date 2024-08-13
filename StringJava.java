38a05f84aed80a856d5a8bc02d2229241f2c7f77
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-13 11:44:14 +0530

    Reverse the words in String

--- StringJava.java
+++ StringJava.java
@@ -1,22 +1,16 @@
 import java.util.Scanner;
-class StringJava{
+class StringFava{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter the String to check");
-		String name = sc.next();
-		
-		//1 
-		for(int i=name.length()-1; i>=0; i--){
-			System.out.print(name.charAt(i));
-		}
-		System.out.println();
+		String str = sc.nextLine();
 
-		//2
-		String name2 = "";
-		for(int i=0; i<name.length(); i++){
-			name2 = name.charAt(i) + name2;
+		String ch[] = str.split(" ");
+		// it will convert array as ch = {"Rushikesh", "Ramesh", "Tekale"};
+		
+		for(int i=ch.length - 1; i>=0; i--){
+			System.out.print(ch[i] + " ");
 		}
-		System.out.println(name2);
 	}
 }
