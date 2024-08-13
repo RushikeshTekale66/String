ec315fd5347d6a78c4c246df3f8e09badbf7b27c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-13 11:47:13 +0530

    Palindrome of the String

--- StringJava.java
+++ StringJava.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
-class StringFava{
+class StringJava{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter the String to check");
 		String str = sc.nextLine();
 
-		String ch[] = str.split(" ");
-		// it will convert array as ch = {"Rushikesh", "Ramesh", "Tekale"};
-		
-		for(int i=ch.length - 1; i>=0; i--){
-			System.out.print(ch[i] + " ");
+		String str2 = "";
+
+		for(int i=str.length()-1; i>=0; i--){
+			str2 = str2 + str.charAt(i);
+		}
+
+		if(str.equals(str2)){
+			System.out.println("Palindrome");
+		}
+		else{
+			System.out.println("Not a palindrome");
 		}
 	}
 }
