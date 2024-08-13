b55f8759cb34a042b678bff321eed59fec49779d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-13 11:39:01 +0530

    Find the count of character in String

--- StringJava.java
+++ StringJava.java
@@ -1,19 +1,18 @@
+import java.util.Scanner;
 class StringJava{
-	public static void main(String[] args) {
-	    //Store in same memory location (String pool)
-	    String s1 = "Rushikesh";
-	    String s2 = "Rushikesh";
-	    
-	    System.out.println("String Pool Elements : ");
-	    System.out.println(s1.equals(s2));
-	    System.out.println(s1==s2);
-	    
-	    //Store in different memory location (Heap)
-	    String s3 = new String("Tekale");
-	    String s4 = new String("Tekale");
-	    
-	    System.out.println("Object in heap memory : ");
-	    System.out.println(s3.equals(s4));
-	    System.out.println(s3==s4);
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the String to check");
+		String name = sc.next();
+		System.out.println("Enter the character to check");
+		char ch = sc.next().charAt(0);
+		int count = 0;
+		for(int i=0; i<name.length(); i++){
+			if(name.charAt(i)==ch){
+				count++;
+			}
+		}
+		System.out.println("Count of Char is " + count);
 	}
 }
