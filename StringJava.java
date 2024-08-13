fd58b6f935c85c3caa1f70bed198b19c7060a33d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-13 11:42:57 +0530

    Rever the character in string

--- StringJava.java
+++ StringJava.java
@@ -5,28 +5,18 @@ class StringJava{
 		
 		System.out.println("Enter the String to check");
 		String name = sc.next();
-		name = name.toLowerCase();
-		int count = 0;
-
-		//Traditional
-		for(int i=0; i<name.length(); i++){
-			if(name.charAt(i)=='a' || name.charAt(i)=='e' || name.charAt(i)=='i' || name.charAt(i)=='o' || name.charAt(i)=='u'){
-				count++;
-			}
+		
+		//1 
+		for(int i=name.length()-1; i>=0; i--){
+			System.out.print(name.charAt(i));
 		}
-		System.out.println("Count of Vovel is " + count);
+		System.out.println();
 
-
-		//Advance Option		
-		String vovel = "aeiou";
-		int count2 = 0;
+		//2
+		String name2 = "";
 		for(int i=0; i<name.length(); i++){
-			for(int j=0; j<vovel.length(); j++){
-				if(name.charAt(i)==vovel.charAt(j)){
-					count2++;
-				}
-			}
-		}	
-		System.out.println("Count of vovel : " + count2);
+			name2 = name.charAt(i) + name2;
+		}
+		System.out.println(name2);
 	}
 }
