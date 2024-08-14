798e2046791ff2504d44f47c51d5fbcaf362f793
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-14 11:54:28 +0530

    Remove special character from string

--- StringJava.java
+++ StringJava.java
@@ -1,19 +1,7 @@
-class StringJava{
-	public static void main(String[] args) {
-		String str = "I am java developer i passionate to java development";
-		
-		//1
-		String [] arr = str.split(" ");
-		for(int i=0; i<arr.length; i++){
-			int count = 0;
-		    for(int j=0; j<arr.length; j++){
-				if(arr[i].equals(arr[j])){
-					count++;
-				}
-			}
-			if(count==1){
-			System.out.println(arr[i]);
-			}
-		}
+class RemoveSpecialChar{
+	public static void main(String [] args){
+		String str = "abcdABCD!@#$1234";
+		str = str.replaceAll("[^A-Za-z0-9]", "");
+		System.out.println(str);
 	}
 }
