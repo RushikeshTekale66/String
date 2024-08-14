1ce77fb63df4aa9f23d5367a6dddc59708c23aed
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-14 11:52:02 +0530

    Remove duplicate character from string

--- StringJava.java
+++ StringJava.java
@@ -1,18 +1,19 @@
 class StringJava{
 	public static void main(String[] args) {
-		String str = "RushikeshTekale";
-		str = str.toLowerCase();
-		String str2 = "ramesh";
-		int index = 8;
-
-		String newStr = "";
-		for(int i=0; i<str.length(); i++){
-			newStr = newStr + str.charAt(i);
-			if(i==index){
-				newStr = newStr + str2;
-			}
-		}	
-		System.out.println(newStr);
+		String str = "rruusshhiik";
 		
+		char [] stra = str.toCharArray();
+		
+		for(int i=0; i<stra.length; i++){
+			int count = 0;
+			for(int j=i; j<stra.length; j++){
+				if(stra[i]==stra[j]){
+					count++;
+				}
+			}
+			if(count==1){
+				System.out.print(stra[i]);
+			}
+		}		
 	}
 }
