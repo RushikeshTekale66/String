0baa8f0cd32b4a41f81e25a63d31d68ab20fd38c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-14 11:55:51 +0530

    Remove the given string from string

--- StringJava.java
+++ StringJava.java
@@ -1,7 +1,21 @@
-class RemoveSpecialChar{
-	public static void main(String [] args){
-		String str = "abcdABCD!@#$1234";
-		str = str.replaceAll("[^A-Za-z0-9]", "");
-		System.out.println(str);
+class RemoveString{
+	public static void main(String[] args) {
+		String str = "Rushikesh Ramesh Tekale";
+		
+		//1
+		String [] arr = str.split(" ");
+		
+		for(int i=0; i<arr.length; i++){
+		    if(!arr[i].equals("Ramesh")){
+		    	System.out.print(arr[i] + " ");
+		    }
+		}
+
+		//2
+		System.out.println();
+		String [] arr2 = str.split("Ramesh");
+		for(int i=0; i<arr2.length; i++){
+			System.out.print(arr2[i]);
+		}
 	}
 }
