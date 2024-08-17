2b640e05d6be1535ff0f5b550dc5bb948557cb0d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-17 10:46:38 +0530

    Print characters of string

--- StringJava.java
+++ StringJava.java
@@ -1,8 +1,10 @@
 class StringJava{
 	public static void main(String[] args) {
 		String str = "adcABC123!@#abcABC!@#$%^&*";
-		str = str.replaceAll("[^a-zA-Z0-9]", "");
-		System.out.println("String after replacing special character : " + str);
+		
+		for(int i=0; i<str.length(); i++){
+			System.out.println(str.charAt(i));
+		}
 		
 	}
 }
