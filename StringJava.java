eddd9ede0a092c18dd1c79bac1737e25d4d5622c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-02 23:54:43 +0530

    Print string in reverse

--- StringJava.java
+++ StringJava.java
@@ -2,8 +2,8 @@ class StringJava{
 	public static void main(String[] args) {
 		String str = "adcABC123!@#abcABC!@#$%^&*";
 		
-		for(int i=0; i<str.length(); i++){
-			System.out.println(str.charAt(i));
+		for(int i=str.length()-1; i>=0; i--){
+			System.out.print(str.charAt(i));
 		}
 		
 	}
