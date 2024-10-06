719c22e984570d810ba0e3f304991e8b096e32f6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-07 00:26:40 +0530

    Last character from string

--- StringJava.java
+++ StringJava.java
@@ -1,16 +1,6 @@
 class StringJava{
 	public static void main(String [] args){
 		String str = "Rushikesh";
-		
-		System.out.println("Duplicate characters are");
-		for(int i=0; i<str.length(); i++){
-			for(int j=i+1; j<str.length(); j++){
-				if(str.charAt(i)==str.charAt(j)){
-					System.out.println(str.charAt(i));
-				}
-			}
-		}
-
-		
+		System.out.println(str.charAt(str.length()-1));		
 	}
 }
